c3d0f74ee7364b66e726433ac09c9426a1219645
kylinzhaoo <117937015+kylinzhaoo@users.noreply.github.com>
2023-01-09 21:47:11 -0500

    added zombie subclass

--- MyWorld.java
+++ MyWorld.java
@@ -13,16 +13,49 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    int count = 0;
+    int spawnSpeed = 50;
+    int randomSpawn;
+    //Spawn speed will be faster if you lower value of spawnSpeed
+    public Player mainPlayer = new Player();
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(700, 600, 1);
+        super(1000, 800, 1);
         
-        addObject(new Player(), getWidth()/2, getHeight()/2);
+        addObject(mainPlayer, getWidth()/2, getHeight()/2);
         /* fills background with the color blue
          * adds character on screen
          * author: kylin
          * date: Dec 20
          */
+    }
+    
+    public void act()
+    {
+        count++;
+        spawnZombies();
+        /* spawns zombies in 8 different spots on the screen
+         * author: kylin zhao
+         * date: Jan 9
+         */
+    }
+    public void spawnZombies()
+    {
+        if (count % spawnSpeed == 0)
+        {
+            randomSpawn = Greenfoot.getRandomNumber(8);
+            switch(randomSpawn){
+                case 0 : addObject(new Zombie(mainPlayer), 0, 0); break;
+                case 1 : addObject(new Zombie(mainPlayer), getWidth()/2, 0); break;
+                case 2 : addObject(new Zombie(mainPlayer), getWidth(), 0); break;
+                case 3 : addObject(new Zombie(mainPlayer), 0, getHeight()/2); break;
+                case 4 : addObject(new Zombie(mainPlayer), getWidth(), getHeight()/2); break;
+                case 5 : addObject(new Zombie(mainPlayer), 0, getHeight()); break;
+                case 6 : addObject(new Zombie(mainPlayer), getWidth()/2, getHeight()); break;
+                case 7 : addObject(new Zombie(mainPlayer), getWidth(), getHeight()); break;
+                
+            }
+        }
     }
 }
---
+++ Zombie.java
@@ -0,0 +1,76 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Zombie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Zombie extends Actor
+{
+    /**
+     * Act - do whatever the Zombie wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int animateImage = 0;
+    int animateSpeed = 5;
+    //lower the animate speed for faster animation
+    int count;
+    int health = 3;
+    Player player;
+    public Zombie(Player mainPlayer)
+    {
+        player = mainPlayer;
+        setImage("skeleton-idle_16.png");
+        getImage().scale(80,80);
+        /* added zombie monster to our game
+         * author: kylin zhao
+         * date: Jan 9
+         */
+    }
+    public void act()
+    {
+        count++;
+        animate();
+        moveAround();
+        hitByProjectile();
+    }
+    //animate our zombie
+    public void animate()
+    {
+        if(count % animateSpeed == 0)
+        {
+            if(animateImage > 16){
+                animateImage = 0;
+            }
+            setImage("skeleton-move_" + animateImage + ".png");
+            animateImage++;
+            getImage().scale(80,80);
+        }
+    }
+    public void moveAround()
+    {
+        move(1);
+        turnTowards(player.getX(), player.getY());
+        /* has zombies target player from spawn
+         * author: kylin zhao
+         * date: Jan 9
+         */
+    }
+    public void hitByProjectile()
+    {
+        Actor projectile = getOneIntersectingObject(Projectile.class);
+        if(projectile != null)
+        {
+            health--;
+            getWorld().removeObject(projectile);
+            
+        }
+        if(health == 0)
+            getWorld().removeObject(this);
+            /* removes the objectile that hits the zombie
+             * author: kylin zhao
+             * date: Jan 9
+             */
+    } 
+}
