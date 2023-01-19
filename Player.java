cf20f8168ea1759a2a7d4ea69392ab1cb24fe280
kylinzhaoo <117937015+kylinzhaoo@users.noreply.github.com>
2023-01-18 23:33:46 -0500

    Revert "Update Player.java"
    This reverts commit f68b8bf2eea38180151aa09efee8dab2c06ab3f9.

--- Player.java
+++ Player.java
@@ -17,6 +17,7 @@ public class Player extends Actor
     WeaponButton weaponButton;
     SuperPower superPower;
     int superTimer;
+    GreenfootSound gunSound = new GreenfootSound("mixkit-game-gun-shot-1662-[AudioTrimmer.com].mp3");
     public Player()
     {
         getImage().scale(80,80);
@@ -67,6 +68,7 @@ public class Player extends Actor
     public void fireProjectile()
     {
         if(Greenfoot.mousePressed(null) && weaponButton.weaponUpgrade == 1){
+            gunSound.play();
             Projectile projectile = new Projectile();
             getWorld().addObject(projectile, getX(), getY()+19);
             projectile.setRotation(getRotation());
